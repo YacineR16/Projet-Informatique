302dc9fe3d3ed640676c7d7d2fa7962373a9aa2c
YacineR16 <cino75019@gmail.com>
2025-03-19 15:49:22 +0100

    Version 1

---
+++ .idea/Projet.py
@@ -0,0 +1,15 @@
+class Drone:
+    def __init__(self,x,y,h):
+        self.position=(x,y)
+        self.altitude=h
+class Tuile :
+    def __init__(self,X,Y,Z,data):
+        self.colonne_tuile=X
+        self.ligne_tuile=Y
+        self.zoom=Z
+        self.data=data
+class AnalyseTuile:
+    def __init__(self):
+
+class Carte :
+    def __init__(self):
